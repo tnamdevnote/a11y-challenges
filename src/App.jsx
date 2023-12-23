8d4d736e29478f0d75839be016ca15ebec71403c
Taek Been Nam <taekbeen93@gmail.com>
2023-12-23 00:03:17 -0500

    style outline

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <main></main>;
+  return (
+    <main className="bg-lighter-blue min-h-screen flex items-start md:items-center">
+      <header className="bg-bg-sidebar-mobile bg-cover fixed top-0 w-full h-[172px] md:hidden"></header>
+      <section className="rounded-xl max-w-[940px] w-full min-h-96 lg:mx-auto my-[99px] md:top-0 drop-shadow-xl mx-8 bg-white"></section>
+      <footer className="bg-white h-[72px] fixed bottom-0 items-center w-full justify-between md:hidden">
+        <button type="button">Next Step</button>
+      </footer>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "bg-sidebar-mobile": "url('/src/assets/images/bg-sidebar-mobile.svg')",
+      },
+      colors: {
+        // Primary
+        "marine-blue": "hsl(213, 96%, 18%)",
+        "purplish-blue": "hsl(243, 100%, 62%)",
+        "pastel-blue": "hsl(228, 100%, 84%)",
+        "light-blue": "hsl(206, 94%, 87%)",
+        "lighter-blue": "hsl(218, 100%, 97%)",
+        "strawberry-red": "hsl(354, 84%, 57%)",
+
+        // Neutral
+        "cool-gray": "hsl(231, 11%, 63%)",
+        "light-gray": "hsl(229, 24%, 87%)",
+        "magnolia:": "hsl(217, 100%, 97%)",
+        alabaster: "hsl(231, 100%, 99%)",
+        "white:": "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
