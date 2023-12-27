2e26bd66bdb01ff9763fe26dd0da7d8caef38ac4
Taek Been Nam <taekbeen93@gmail.com>
2023-12-26 23:49:38 -0500

    pass form data to perosnal info

--- src/App.tsx
+++ src/App.tsx
@@ -4,61 +4,39 @@ import SelectPlanForm from "./components/forms/SelectPlanForm";
 import AddOnsForm from "./components/forms/AddOnsForm";
 import SummaryForm from "./components/forms/SummaryForm";
 import { useState } from "react";
+import React from "react";
 
-const FORM_DATA = {
+type FormData = {
+  name: string;
+  email: string;
+  phone: string;
+  planType: "monthly" | "yearly";
+  plan: { id: string; label: string; price: number };
+  addOns?: { id: string; label: string; price: number }[];
+};
+
+const INITIAL_DATA = {
   name: "",
   email: "",
   phone: "",
-  monthlyPlan: [
-    {
-      id: "arcade-month",
-      label: "Arcade",
-      price: 9,
-    },
-    {
-      id: "advanced-month",
-      label: "Advanced",
-      price: 12,
-    },
-    {
-      id: "pro-month",
-      label: "Pro",
-      price: 15,
-    },
-  ],
-  yearlyPlan: [
-    {
-      id: "arcade-year",
-      label: "Arcade",
-      price: 90,
-    },
-    {
-      id: "advanced-year",
-      label: "Advanced",
-      price: 120,
-    },
-    {
-      id: "pro-year",
-      label: "Pro",
-      price: 150,
-    },
-  ],
-  addOns: [
-    {
-      id: "online-service",
-      label: "Online service",
-    },
-  ],
+  planType: "monthly",
+  plan: {},
 };
 
 function App() {
-  const [formData, setFormData] = useState(FORM_DATA);
+  const [formData, setFormData] = useState(INITIAL_DATA);
+  const updateForm = (fields: Partial<FormData>) => {
+    setFormData((prev) => {
+      return { ...prev, ...fields };
+    });
+  };
+
   const { steps, currentStepIndex, currentStep, isStart, isLast, next, back } =
     useMultiStepForm([
-      <YourInfoForm data={formData} />,
-      <SelectPlanForm data={formData} />,
-      <AddOnsForm data={formData} />,
-      <SummaryForm data={formData} />,
+      <YourInfoForm {...formData} onChange={updateForm} />,
+      <SelectPlanForm />,
+      <AddOnsForm />,
+      <SummaryForm />,
     ]);
 
   return (
@@ -79,7 +57,21 @@ function App() {
         ))}
       </div>
       <section className="mx-8 my-[99px] min-h-96 w-full max-w-[940px] rounded-xl bg-white drop-shadow-xl md:flex lg:mx-auto">
-        <div className="md:bg-bg-sidebar-desktop m-4 mr-0 hidden h-[568px] w-[274px] flex-shrink-0 md:block"></div>
+        <div className="md:bg-bg-sidebar-desktop m-4 mr-0 hidden h-[568px] w-[274px] flex-shrink-0 md:flex md:flex-col">
+          {steps.map((_, i) => (
+            <button
+              type="button"
+              className={`h-8 w-8 rounded-full p-0 ${
+                currentStepIndex === i
+                  ? "bg-light-blue text-marine-blue ring-0"
+                  : "text-white ring-1 ring-white"
+              }`}
+            >
+              {i + 1}
+              <span></span>
+            </button>
+          ))}
+        </div>
         <form
           id="multi-step_form"
           className="p-8 md:flex md:w-full md:flex-col md:justify-between md:px-20 md:pt-16"
@@ -88,7 +80,7 @@ function App() {
           <div className="hidden md:flex md:justify-end">
             {!isStart && (
               <button
-                className="mr-auto rounded-md text-lg text-cool-gray transition-colors hover:text-marine-blue"
+                className="btn-no-fill mr-auto rounded-md text-lg"
                 type="button"
                 form="multi-step_form"
                 onClick={back}
@@ -98,7 +90,7 @@ function App() {
             )}
             {isLast ? (
               <button
-                className="rounded-md bg-purplish-blue px-6 text-lg text-white transition-colors hover:bg-purplish-blue/80"
+                className="btn-secondary rounded-md px-6 text-lg"
                 type="button"
                 onClick={next}
               >
@@ -106,7 +98,7 @@ function App() {
               </button>
             ) : (
               <button
-                className="rounded-md bg-marine-blue px-6 text-lg text-white transition-colors hover:bg-marine-blue/80"
+                className="btn-primary rounded-md px-6 text-lg"
                 type="button"
                 onClick={next}
               >
@@ -119,7 +111,7 @@ function App() {
       <footer className="fixed bottom-0 flex h-[72px] w-full items-center justify-end bg-white p-8 md:hidden">
         {!isStart && (
           <button
-            className="mr-auto rounded-sm text-base text-cool-gray transition-colors hover:text-marine-blue"
+            className="btn-no-fill mr-auto rounded-sm text-base"
             type="button"
             form="multi-step_form"
             onClick={back}
@@ -129,7 +121,7 @@ function App() {
         )}
         {isLast ? (
           <button
-            className="rounded-sm bg-purplish-blue px-4 text-base text-white transition-colors hover:bg-purplish-blue/80"
+            className="btn-secondary rounded-sm px-4 text-base"
             type="button"
             form="multi-step_form"
             onClick={next}
@@ -138,7 +130,7 @@ function App() {
           </button>
         ) : (
           <button
-            className="rounded-sm bg-marine-blue px-4 text-base text-white transition-colors hover:bg-marine-blue/80"
+            className="btn-primary rounded-sm px-4 text-base"
             type="button"
             form="multi-step_form"
             onClick={next}
--- src/components/forms/YourInfoForm.tsx
+++ src/components/forms/YourInfoForm.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import FormWrapper from "./FormWrapper";
 
-function YourInfoForm() {
+type YourInfoProps = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+type YourInfoFormProps = YourInfoProps & {
+  onChange: (field: Partial<YourInfoProps>) => void;
+};
+
+function YourInfoForm({ name, email, phone, onChange }: YourInfoFormProps) {
+  const handleChange = (
+    field: string,
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    onChange({ [field]: e.target.value });
+  };
   return (
     <FormWrapper
       formId="multi-step-form"
@@ -14,7 +30,9 @@ function YourInfoForm() {
           className="h-10 rounded-sm  px-4 py-2 font-ubuntuMedium text-[15px] ring-1 ring-light-gray focus-visible:ring-purplish-blue"
           type="text"
           placeholder="e.g. Luke Nam"
+          value={name}
           maxLength={50}
+          onChange={(e) => handleChange("name", e)}
         />
       </label>
       <label className="flex flex-col gap-1 text-xs text-marine-blue">
@@ -23,6 +41,8 @@ function YourInfoForm() {
           className="h-10 rounded-sm px-4 py-2 font-ubuntuMedium text-[15px] ring-1 ring-light-gray focus-visible:ring-purplish-blue"
           type="email"
           placeholder="e.g. lukenam@lorem.com"
+          value={email}
+          onChange={(e) => handleChange("email", e)}
         />
       </label>
       <label className="flex flex-col gap-1 text-xs text-marine-blue">
@@ -32,6 +52,8 @@ function YourInfoForm() {
           type="tel"
           placeholder="e.g. +1 616-234-5678"
           maxLength={50}
+          value={phone}
+          onChange={(e) => handleChange("phone", e)}
         />
       </label>
     </FormWrapper>
